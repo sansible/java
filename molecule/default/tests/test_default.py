2885a3c307a4c0714642a12951cb15b9c58530e4
David Reed <david.reedfrancis@sainsburys.co.uk>
2019-05-16 11:42:50 +0100

    [GITHUB-16] Updates to pool based keyserver
    Switches from slightly flakey ubuntu keyserver to the pool based
    eu sks one, also updates with latest versions of deps and Makefile.
    
    Also removes Oracle installation, there are no "official" PPAs
    for this now as Oracle have changed their license.
    
    Renames the java8 scenario to default, not having a default
    seems to confuse Molecule.

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -10,9 +10,6 @@ def test_java_versions(host):
     java8openjdk = host.package("openjdk-8-jdk")
     assert java8openjdk.is_installed
 
-    java8oracle = host.package("oracle-java8-installer")
-    assert java8oracle.is_installed
-
 
 def test_java_default(host):
     java_dest = "/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java"
